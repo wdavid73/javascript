75193f4af7a63a8a5491b94bddf3e2a556be7a81
WDavidPj <wilsonpadilla2014@gmail.com>
2019-04-08 19:04:13 -0500

    haciendo matrix

---
+++ matrix/matrix.js
@@ -0,0 +1,7 @@
+export const Matrix = (size) =>{
+if(size.length % 3 == 0 ){
+    size_matrix = 3;
+}else if(size.length % 2 == 0){
+    size_matrix = 2;
+}
+}/*haciendo logica para armar la matrix falta por terminar*/
